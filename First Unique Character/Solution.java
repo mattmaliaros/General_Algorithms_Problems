8b50f82855b15bc756f28855fecc747c1717af42
MATTHEW MALIAROS <42752582+mattmaliaros@users.noreply.github.com>
2021-06-14 11:46:59 -0400

    Update Solution.java

--- First Unique Character/Solution.java
+++ First Unique Character/Solution.java
@@ -27,3 +27,30 @@ class Solution {
         return -1;
     }
 }
+
+/*          GET OR DEFAULT
+
+class Solution {
+    public int firstUniqChar(String s) {
+        if(s.length() == 1) return 0;
+        return helper(s, new HashMap<>());
+    }
+    public static int helper(String testCase, HashMap<Character, Integer> countOfChars)
+        {
+        int end = testCase.length()-1;
+        while (end >= 0) {
+            Character currentChar = testCase.charAt(end);
+            countOfChars.put(currentChar, countOfChars.getOrDefault(currentChar, 0) + 1);
+               end--;
+        }
+        int start = 0;
+        while(start <= testCase.length()-1) {
+            if(countOfChars.get(testCase.charAt(start)) == 1 ) {
+                return start;
+            }
+            start++;
+        }
+        return -1;
+    }
+}
+*/
